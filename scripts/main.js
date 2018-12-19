f1c2d78655c5624d57ebabb09aecf7409a1a766d
DmitryYevtushenko <yevtushenkods@gmail.com>
2018-12-19 20:44:01 +0200

    doing exercises

--- scripts/main.js
+++ scripts/main.js
@@ -436,3 +436,30 @@
 // var mySecondString = stArray.toString(); //delimiter is ',' only
 
 /*==========Printing those products==========*/
+var list = document.querySelector('.output ul');
+var totalBox = document.querySelector('.output p');
+var total = 0;
+list.innerHTML = '';
+totalBox.textContent = '';
+// number 1 - do an array
+                'Underpants:6.99'
+                'Socks:5.99'
+                'T-shirt:14.99'
+                'Trousers:31.99'
+                'Shoes:23.99';
+
+for (var i = 0; i <= 0; i++) {
+  // number 2 - change the condition to array's length
+  // number 3
+
+  // number 4
+  
+  // number 5
+  itemText = 0;
+  
+  var listItem = document.createElement('li');
+  listItem.textContent = itemText;
+  list.appendChild(listItem);
+}
+
+totalBox.textContent = 'Total: $' + total.toFixed(2);
