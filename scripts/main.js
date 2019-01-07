d8cda4dd978cb5b31f8c70eaf7b067b3bda8f34d
Dmitry <dmitry.yev@gmail.com>
2019-01-07 02:31:57 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -465,3 +465,41 @@ for (var i = 0; i < products.length; i++) {
 }
 totalBox.textContent = 'Total: $' + total.toFixed(2);
 /*======= END Printing those products=======*/
+
+/*==========Top 5 searches==========*/
+var list = document.querySelector('.output ul');
+var searchInput = document.querySelector('.output input');
+var searchBtn = document.querySelector('.output button');
+list.innerHTML = '';
+var myHistory = [];
+
+searchBtn.onclick = function() {
+  // we will only allow a term to be entered if the search input isn't empty
+    if ((searchInput.value !== '')&&(searchInput.value !== myHistory[0])&&(searchInput.value !== myHistory[1])&&(searchInput.value !== myHistory[2])&&(searchInput.value !== myHistory[3])&&(searchInput.value !== myHistory[4])) {
+      // number 1
+      myHistory.unshift(searchInput.value);
+      // empty the list so that we don't display duplicate entries
+      // the display is regenerated every time a search term is entered.
+      list.innerHTML = '';
+
+      // loop through the array, and display all the search terms in the list
+      for (var i = 0; i < myHistory.length; i++) {
+        itemText = myHistory[i];
+        var listItem = document.createElement('li');
+        listItem.textContent = itemText;
+        list.appendChild(listItem);
+      }
+
+      // If the array length is 5 or more, remove the oldest search term
+      if (myHistory.length >= 5) {
+        // number 2
+        myHistory.pop();
+      }
+
+      // empty the search input and focus it, ready for the next term to be entered
+      searchInput.value = '';
+      searchInput.focus();
+    }
+  
+}
+/*==========END Top 5 searches==========*/
