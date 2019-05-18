fd888576738d523d502cea72efc906d78b719213
Dmitry <dmitry.yev@gmail.com>
2019-05-18 18:21:29 +0300

    cleaned the main.js file

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,2 @@
 'use strict';
 
-var animal = {
-  jumps: null
-};
-var rabbit = {
-  jumps: true
-};
-
-rabbit.__proto__ = animal;
-
-alert(rabbit.jumps); // ? (1)
-
-delete rabbit.jumps;
-
-alert(rabbit.jumps); // ? (2)
-
-delete animal.jumps;
-
-alert(rabbit.jumps); // ? (3)
