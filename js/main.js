56bf7f2d5074d04c8054df3802cabdfd5ab19cc8
Dmitry Yevtushenko <yevtushenkods@gmail.com>
2019-09-24 19:38:28 +0300

    Learning: realized array of users obj to array of user's names

--- js/main.js
+++ js/main.js
@@ -1,31 +1,13 @@
 'use strict';
 
-function Calculator() {
-  let operations = {
-    '+': (a, b) => (a + b),
-    '-': (a, b) => (a - b),
-  };
+let vasya = {name: 'Vasiliy', age: 25};
+let petya = {name: 'Petr', age: 33};
+let masha = {name: 'Masha', age: 23};
 
-  this.calculate = (str) => {
-    let strToArray = str.split(' '),
-        a = +strToArray[0],
-        b = +strToArray[2],
-        op = strToArray[1];
+let users = [vasya, petya, masha];
 
-    if (!operations[op]) return NaN;
+let names = users.map(
+    (item) => item.name
+);
 
-    return operations[op](a, b);
-  };
-
-  this.addMethod = (name, func) => {
-    operations[name] = func;
-  }
-}
-
-let calc = new Calculator;
-let powerCalc = new Calculator;
-powerCalc.addMethod("*", (a, b) => a * b);
-powerCalc.addMethod("/", (a, b) => a / b);
-powerCalc.addMethod("**", (a, b) => a ** b);
-
-console.log( powerCalc.calculate('3 * 7') );
+console.log(names);
